87739f494212da0fdd001a3994994a3fc6b0bed1
Paul Ly <paul.ly@live.ca>
2021-03-29 18:22:48 -0400

    Finished Application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, {useEffect} from 'react';
 
-import './App.css';
-import 'materialize-css/dist/css/materialize.min.css';
-import M from'materialize-css/dist/js/materialize.min.js';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
+
+import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal'
 
 const App = () => {
   useEffect(() => {
@@ -12,12 +22,19 @@ const App = () => {
     M.AutoInit();
   });
   return (
-    <React.Fragment>
-      <SearchBar/>
-      <div className="container">
-        <Logs/>
-      </div>
-    </React.Fragment>
+    <Provider store={store}>
+      <React.Fragment>
+        <SearchBar/>
+          <div className="container">
+            <AddBtn/>
+            <AddLogModal/>
+            <EditLogModal/>
+            <AddTechModal/>
+            <TechListModal/>
+          <Logs/>
+        </div>
+      </React.Fragment>
+    </Provider>
   );
 }
 
--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,26 +1,18 @@
-import React ,{useState, useEffect} from 'react'
+import React ,{ useEffect} from 'react'
+import { connect } from 'react-redux';
+import LogItem from './LogItem'
+import Preloader from '../layout/Preloader'
+import PropTypes from 'prop-types'
+import { getLogs } from '../../actions/logActions';
 
-const Logs = () => {
-    const [logs, setLogs] = useState([]);
-    const [loading, setLoading] = useState(false);
-
-
+const Logs = ({ log: { logs, loading }, getLogs }) => {
     useEffect(() => {
-        getLogs();
-        //eslint-disable-next-line
+      getLogs();
+      // eslint-disable-next-line  
     }, []);
 
-    const getLogs = async () => {
-        setLoading (true);
-        const res = await fetch('/logs');
-        const data = await res.json();
-
-        setLogs(data);
-        setLoading(false);
-    }
-
-    if (loading) {
-        return <h4>Loading...</h4>
+    if (loading || logs === null) {
+       return <Preloader/>;
     }
     return (
         <ul className="collection with-header">
@@ -30,10 +22,22 @@ const Logs = () => {
             { !loading && logs.length === 0 ? (
                 <p className="center">No logs to show...</p>
             ) : (
-                logs.map(log => <li>{log.message}</li>)
+                logs.map(log => <LogItem log={log} key={log.id}/>)
             )}
         </ul>
     );
 }
 
-export default Logs
+Logs.propTypes = {
+    log: PropTypes.object.isRequired,
+}
+
+const mapStateToProps = state => ({
+    log: state.log,
+    getLogs: PropTypes.func.isRequired,
+})
+//Note: import action as props
+export default connect( 
+    mapStateToProps, 
+    { getLogs }
+) (Logs);
